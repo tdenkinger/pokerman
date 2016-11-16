e595dee1bfe632cac4ea7f84f31160e8df6f0b2f
Troy Denkinger <tdenkinger@gmail.com>
2016-11-16 08:03:47 -0500

    Full initial player state set.

--- lib/gen_player.ex
+++ lib/gen_player.ex
@@ -7,18 +7,28 @@ defmodule GenPlayer do
     GenServer.start_link(__MODULE__, player_name, name: Player)
   end
 
-  def name(pid) do
-    GenServer.call(pid, {:name})
-  end
+  def name(pid), do: GenServer.call(pid, {:name})
+
+  def stack(pid), do: GenServer.call(pid, {:stack})
+
+  def cards(pid), do: GenServer.call(pid, {:cards})
 
   # Callbacks
 
   def init(player_name) do
-    {:ok, %{name: player_name}}
+    {:ok, %{name: player_name, stack: 0, cards: []}}
   end
 
   def handle_call({:name}, _from, player_details) do
     {:reply, player_details[:name], player_details}
+  end
+
+  def handle_call({:stack}, _from, player_details) do
+    {:reply, player_details[:stack], player_details}
+  end
+
+  def handle_call({:cards}, _from, player_details) do
+    {:reply, player_details[:cards], player_details}
   end
 
 
--- test/gen_player_test.exs
+++ test/gen_player_test.exs
@@ -9,6 +9,8 @@ defmodule GenPlayerTest do
 
   test "stores a player's name", %{player: player} do
     assert GenPlayer.name(player) == "Troy"
+    assert GenPlayer.stack(player) == 0
+    assert GenPlayer.cards(player) == []
   end
 end
 
