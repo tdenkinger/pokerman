007fbd93031226f42e8ce88036f7694f1a0e9df8
Troy Denkinger <tdenkinger@gmail.com>
2016-11-18 07:12:47 -0500

    Use pattern matching to extract data from the player details and just pass atoms instead of tuples.

--- lib/gen_player.ex
+++ lib/gen_player.ex
@@ -7,11 +7,11 @@ defmodule GenPlayer do
     GenServer.start_link(__MODULE__, player_name, [])
   end
 
-  def name(pid), do: GenServer.call(pid, {:name})
+  def name(pid), do: GenServer.call(pid, :name)
 
-  def stack(pid), do: GenServer.call(pid, {:stack})
+  def stack(pid), do: GenServer.call(pid, :stack)
 
-  def cards(pid), do: GenServer.call(pid, {:cards})
+  def cards(pid), do: GenServer.call(pid, :cards)
 
   def buy_chips(pid, amount), do: GenServer.cast(pid, {:buy_chips, amount})
 
@@ -25,34 +25,29 @@ defmodule GenPlayer do
     {:ok, %{name: player_name, stack: 0, cards: {nil, nil}}}
   end
 
-  def handle_cast({:buy_chips, amount}, player_details) do
-    {:noreply, adjust_stack(player_details, amount)}
+  def handle_cast({:buy_chips, amount}, player) do
+    {:noreply, adjust_stack(player, amount)}
   end
 
-  def handle_cast({:deal_cards, card1, card2}, player_details) do
-    {:noreply, update_cards(player_details, {card1, card2})}
+  def handle_cast({:deal_cards, card1, card2}, player) do
+    {:noreply, update_cards(player, {card1, card2})}
   end
 
-  def handle_call({:bet, amount}, _from, player_details) do
-    player_details  = adjust_stack(player_details, -amount)
-    {:reply, {:ok, player_details[:stack]}, player_details}
+  def handle_call({:bet, amount}, _from, player) do
+    player = adjust_stack(player, -amount)
+    {:reply, {:ok, player[:stack]}, player}
   end
 
-  def handle_call({:name}, _from, player_details) do
-    {:reply, player_details[:name], player_details}
-  end
+  def handle_call(:name,  _from, %{name:  name}  = player), do: {:reply, name,  player}
 
-  def handle_call({:stack}, _from, player_details) do
-    {:reply, player_details[:stack], player_details}
-  end
+  def handle_call(:stack, _from, %{stack: stack} = player), do: {:reply, stack, player}
 
-  def handle_call({:cards}, _from, player_details) do
-    {:reply, player_details[:cards], player_details}
-  end
+  def handle_call(:cards, _from, %{cards: cards} = player), do: {:reply, cards, player}
 
   # Helper functions
-  defp update_cards(player_details, cards) do
-    Map.put(player_details, :cards, cards)
+
+  defp update_cards(player, cards) do
+    Map.put(player, :cards, cards)
   end
 
   defp adjust_stack(player, amount) do
